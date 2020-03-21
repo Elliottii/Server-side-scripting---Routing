40db9c4fa0a6fe00ec543b2d7e66691c29487bbe
Eeli Klemettilä <eeli.klemettila@gmail.com>
2020-03-21 14:05:58 +0200

    added car routes

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,25 @@
 'use strict';
-// catRoute
+const express = require('express');
+const router = express.Router();
+
+router.route('/')
+    .get((req, res) => {
+        res.send('From this endpoint you can get cats.')
+    })
+    .post((req, res) => {
+        res.send('With this endpoint you can add cats.')
+    })
+    .put((req, res) => {
+        res.send('With this endpoint you can edit cats.')
+    })
+    .delete((req, res) => {
+        res.send('With this endpoint you can delete cats.')
+    });
+
+router.route('/:id')
+    .get((req, res) => {
+    res.send('You reqested a cat whose id is ' + req.params.id)
+});
+
+module.exports = router;
+
